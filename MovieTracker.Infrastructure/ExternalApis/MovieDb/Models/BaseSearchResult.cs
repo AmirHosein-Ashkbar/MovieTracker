5e676f4111e9ef573b4fe4116090f9e5ad4b783f
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:38:07 +0330

    ExternalApis Models added

---
+++ MovieTracker.Infrastructure/ExternalApis/MovieDb/Models/BaseSearchResult.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTracker.Infrastructure.ExternalApis.MovieDb.Models;
+
+public class BaseSearchResult<T>
+{
+    public int Page { get; set; }
+    public List<T> Results { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalResults { get; set; }
+}
