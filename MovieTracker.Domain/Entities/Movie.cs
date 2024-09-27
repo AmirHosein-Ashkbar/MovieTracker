cfa4d1eeea164af17f59d7b0caaa200f27046252
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:32:30 +0330

    Adding Entities

--- MovieTracker.Domain/Entities/Movie.cs
+++ MovieTracker.Domain/Entities/Movie.cs
@@ -8,7 +8,7 @@ namespace MovieTracker.Domain.Entities;
 public class Movie
 {
     public Guid Id { get; set; }
-    public string DbId { get; set; }
+    public string ExternalId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public int Duration { get; set; }
