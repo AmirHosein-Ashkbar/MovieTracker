06d7e00f17a982e4be4a40dd8ceabfa87ef99d9d
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:33:49 +0330

    ServiceCollectionExtension added

---
+++ MovieTracker.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTracker.API.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+}
