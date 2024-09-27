2c53f8b0cd21ea9cf069ce91034bf2dc3c3889cf
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:33:16 +0330

    MoviesController added

---
+++ MovieTracker.API/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MovieTracker.Application.UseCases.Movie.Queries.GetMovieByName;
+using System.Net.Mime;
+
+namespace MovieTracker.API.Controllers;
+
+public class MoviesController(IMediator mediatr) : BaseController
+{
+    [HttpGet]
+    [Route("getall")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get([FromQuery] string name)
+    {
+        var result = await mediatr.Send(new GetMovieByNameQuery(name));
+        return Ok(result);
+    }
+}
