2bbbc397c8bd6ed928609a7ede1b6ac01dda652f
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:36:19 +0330

    ExternalApis ApiCall Layer added

---
+++ MovieTracker.Infrastructure/ExternalApis/MovieDb/ApiCall/IMovieDbApiCall.cs
@@ -0,0 +1,10 @@
+﻿using MovieTracker.Infrastructure.ExternalApis.MovieDb.Models;
+using RestSharp;
+
+
+namespace MovieTracker.Infrastructure.ExternalApis.MovieDb.ApiCall;
+public interface IMovieDbApiCall
+{
+    Task<BaseSearchResult<TMDBMovieSearch>> GetMovieByName(string name);
+
+}
---
+++ MovieTracker.Infrastructure/ExternalApis/MovieDb/ApiCall/MovieDbApiCall.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Azure.Core.Pipeline;
+using Microsoft.Extensions.Options;
+using MovieTracker.Infrastructure.ExternalApis.MovieDb.Models;
+using MovieTracker.Infrastructure.Settings;
+using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MovieTracker.Infrastructure.ExternalApis.MovieDb.ApiCall;
+public class MovieDbApiCall : IMovieDbApiCall
+{
+    private MovieDbSettings _settings;
+    private RestClientOptions _clientOptions;
+
+    public MovieDbApiCall(IOptionsSnapshot<MovieDbSettings> settings)
+    {
+        _settings = settings.Value;
+
+        _clientOptions = new RestClientOptions(_settings.BaseUrl)
+        {
+            Authenticator = new JwtAuthenticator(_settings.Token)
+        };
+    }
+    public async Task<BaseSearchResult<TMDBMovieSearch>> GetMovieByName(string name)
+    {
+        var client = new RestClient(_clientOptions);
+        var request = new RestRequest($"search/movie");
+        request.AddQueryParameter("query", name);
+        request.AddQueryParameter("include_adult", false);
+        try
+        {
+            var response = await client.GetAsync(request);
+            var result = JsonConvert.DeserializeObject<BaseSearchResult<TMDBMovieSearch>>(response.Content);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        
+    }
+}
