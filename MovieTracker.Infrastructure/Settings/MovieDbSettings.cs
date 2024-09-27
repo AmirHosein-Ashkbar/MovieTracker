2cd149c2234a8c8a1fa49c1f95c5006a1967d149
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:32:11 +0330

    add MovieDbSettings

---
+++ MovieTracker.Infrastructure/Settings/MovieDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.Infrastructure.Settings;
+public class MovieDbSettings
+{
+    public static readonly string Name = "MovieDbSettings";
+    public string BaseUrl { get; set; }
+    public string Token { get; set; }
+    public string ImageBaseUrl { get; set; }
+}
