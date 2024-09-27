c965d0dd90fc7363a45fb1d776f2512254bf31e4
AmirHosein Ashkbar <129063168+AmirHosein-Ashkbar@users.noreply.github.com>
2024-09-27 17:34:18 +0330

    Application Contracts

---
+++ MovieTracker.Application/Contracts/ExternalApisServices/IMovieDbApiService.cs
@@ -0,0 +1,7 @@
+﻿using MovieTracker.Application.UseCases.Movie.Dtos;
+
+namespace MovieTracker.Application.Contracts.ExternalApisServices;
+public interface IMovieDbApiService
+{
+    Task<List<MovieSearchDto>> GetMovieByName(string name);
+}
