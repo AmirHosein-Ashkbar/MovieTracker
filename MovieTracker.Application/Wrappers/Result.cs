f13e2c592a9990f955271cdf71826045a08d8d8c
AmirHoseinAshkbar <amir.h.ashkbar81@gmail.com>
2024-09-29 18:16:46 +0330

    Result Pattern Unfinished

---
+++ MovieTracker.Application/Wrappers/Result.cs
@@ -0,0 +1,33 @@
+﻿
+namespace MovieTracker.Application.Wrappers;
+
+public class Result<T>
+{
+    private readonly T? _data;
+    private readonly string _message;
+    private readonly List<string> _errors;
+    private readonly bool _isSuccess;
+
+    private Result(T data)
+    {
+        _isSuccess = false;
+        _data = data;
+        _message = string.Empty;
+        _errors = new List<string>();
+    }
+
+    private Result(List<string> error)
+    {
+        _isSuccess = false;
+        _data = default;
+        _message = string.Empty;
+        _errors = new List<string>();
+    }
+
+    public static Result<T> Success(T data)
+    {
+
+        return new Result<T>(data);
+    }
+
+}
