b592e08a8879cf3b3c2f2642030c4bacde4e8838
AmirHoseinAshkbar <amir.h.ashkbar81@gmail.com>
2024-10-05 16:49:53 +0330

    Merging Queries and QueryHandlers

--- MovieTracker.Application/UseCases/Movie/Queries/GetMovieByName/GetMovieByNameQuery.cs
+++ MovieTracker.Application/UseCases/Movie/Queries/GetMovieByName/GetMovieByNameQuery.cs
@@ -1,6 +1,20 @@
 ﻿using MediatR;
+using MovieTracker.Application.Contracts.ExternalApisServices;
 using MovieTracker.Application.UseCases.Movie.Dtos;
 
 
 namespace MovieTracker.Application.UseCases.Movie.Queries.GetMovieByName;
 public record GetMovieByNameQuery( string name ) : IRequest<List<MovieSearchDto>>;
+
+
+
+public class GetMovieByNameQueryHandler(ITMDBApiService movieDbService) : IRequestHandler<GetMovieByNameQuery, List<MovieSearchDto>>
+{
+    public async Task<List<MovieSearchDto>> Handle(GetMovieByNameQuery request, CancellationToken cancellationToken)
+    {
+        var response = await movieDbService.GetMovieByName(request.name);
+
+
+        return response;
+    }
+}
--- MovieTracker.Application/UseCases/Movie/Queries/GetMovieDetailsById/GetMovieDetailsByIdQuery.cs
+++ MovieTracker.Application/UseCases/Movie/Queries/GetMovieDetailsById/GetMovieDetailsByIdQuery.cs
@@ -1,8 +1,18 @@
-﻿using ErrorOr;
-using MediatR;
+﻿using MediatR;
+using MovieTracker.Application.Contracts.ExternalApisServices;
 using MovieTracker.Application.UseCases.Movie.Dtos;
 
 namespace MovieTracker.Application.UseCases.Movie.Queries.GetMovieDetailsById;
 
-public record GetMovieDetailsByIdQuery(int Id) : IRequest<MovieDetailsDto> ; 
+public record GetMovieDetailsByIdQuery(int Id) : IRequest<MovieDetailsDto>;
+
+
+public class GetMovieDetailsByIdQueryHandler(ITMDBApiService TMDBApiService) : IRequestHandler<GetMovieDetailsByIdQuery, MovieDetailsDto>
+{
+    public async Task<MovieDetailsDto> Handle(GetMovieDetailsByIdQuery request, CancellationToken cancellationToken)
+    {
+        var response = await TMDBApiService.GetMovieDetailsById(request.Id);
+        return response;
+    }
+}
 
