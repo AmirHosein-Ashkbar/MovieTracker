934339c2d2e2cacfe4ed09f629f07f7ac9c0d127
AmirHoseinAshkbar <amir.h.ashkbar81@gmail.com>
2024-10-05 16:40:29 +0330

    Improved Errors

--- MovieTracker.Application/Errors/Error.cs
+++ MovieTracker.Application/Errors/Error.cs
@@ -1,9 +1,11 @@
 ﻿
+using Microsoft.AspNetCore.Http;
+
 namespace MovieTracker.Application.Errors;
 
 public class Error
 {
-    private Error(string code, string description, ErrorType type)
+    private Error(string code, string description, ErrorType type, int statusCode)
     {
         Code = code;
         Description = description;
@@ -15,10 +17,25 @@ public class Error
     public string StatusCode { get; set; }
 
     public static Error Unexpected(string code = "Unexpected", string description = "An unexpected error happend.")
-        => new Error(code, description, ErrorType.Unexpected);
+        => new Error(code, description, ErrorType.Unexpected, StatusCodes.Status500InternalServerError);
 
     public static Error Validation(string code = "Validation", string description = "A one or more validation error happend.")
-        => new Error(code, description, ErrorType.Unexpected);
+        => new Error(code, description, ErrorType.Validation, StatusCodes.Status400BadRequest);
+
+    public static Error Failure(string code = "Failure", string description = "A failure happend.")
+    => new Error(code, description, ErrorType.Failure, StatusCodes.Status500InternalServerError);
+
+    public static Error NotFound(string code = "NotFound", string description = "The requested resource not found.")
+    => new Error(code, description, ErrorType.NotFound, StatusCodes.Status404NotFound);
+
+    public static Error Unauthorized(string code = "Unauthorized", string description = "You're not authorized.")
+    => new Error(code, description, ErrorType.Unauthorized, StatusCodes.Status401Unauthorized);
+
+    public static Error Forbidden(string code = "Forbidden", string description = "You're Forbidden.")
+    => new Error(code, description, ErrorType.Forbidden, StatusCodes.Status403Forbidden);
+
+    public static Error Conflict(string code = "Conflict", string description = "Conflict.")
+   => new Error(code, description, ErrorType.Forbidden, StatusCodes.Status409Conflict);
 
 }
 public enum ErrorType
