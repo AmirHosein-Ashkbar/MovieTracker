b1ff2ea614afb3d25b0a16bc9f97a57b8ba87daa
amir.h.ashkbar <amir.h.ashkbar@gmail.com>
2024-10-11 23:26:23 +0330

    adding fluent validation

---
+++ MovieTracker.Application/UseCases/Movie/Queries/GetMovieByName/GetMovieByNameValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+
+namespace MovieTracker.Application.UseCases.Movie.Queries.GetMovieByName;
+public class GetMovieByNameValidator : AbstractValidator<GetMovieByNameQuery>
+{
+    public GetMovieByNameValidator()
+    {
+        RuleFor(x => x.name).NotEmpty().NotNull();    
+    }
+}
