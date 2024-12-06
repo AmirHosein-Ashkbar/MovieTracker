0b874d4794cf53f7e316744a6220a33bd6c0fe1d
alireza <iamalirezaashkbar@gmail.com>
2024-12-06 19:31:45 +0330

    Add BaseEntity

---
+++ MovieTracker.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieTracker.Domain.Entities;
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public bool IsActive { get; set; } = true;
+
+}
--- MovieTracker.Domain/Entities/Movie.cs
+++ MovieTracker.Domain/Entities/Movie.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieTracker.Domain.Entities;
-public class Movie
+﻿namespace MovieTracker.Domain.Entities;
+public class Movie : BaseEntity
 {
-    public Guid Id { get; set; }
     public string ExternalId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
