064f101e6844ea02bac31c28618b2c05a5d430e2
Amirhosein <amir.h.ashkbar@gmail.com>
2024-12-06 23:41:29 +0330

    validation pipeline unfinished

--- MovieTracker.Application/Behaviours/ValidationPipelineBehaviour.cs
+++ MovieTracker.Application/Behaviours/ValidationPipelineBehaviour.cs
@@ -3,32 +3,58 @@ using FluentValidation;
 using System.ComponentModel.DataAnnotations;
 using MovieTracker.Application.Wrappers;
 using MovieTracker.Application.Errors;
+using FluentValidation.Results;
+using System.Threading;
+using System.Reflection;
 
 namespace MovieTracker.Application.Behaviours;
 
 public class ValidationPipelineBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
     : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull, IRequest<TResponse>
-    where TResponse : Result<TResponse>
+    where TRequest : class
+    where TResponse : Result
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        var context = new ValidationContext<TRequest>(request);
-        if (validators.Any()) 
+        ArgumentNullException.ThrowIfNull(next);
+
+        if (validators.Any())
         {
+            var context = new ValidationContext<TRequest>(request);
+
             var validationResults = await Task.WhenAll(
-               validators.Select(v =>
-                   v.ValidateAsync(context, cancellationToken))).ConfigureAwait(false);
+                validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken))).ConfigureAwait(false);
 
             var failures = validationResults
                 .Where(r => r.Errors.Count > 0)
                 .SelectMany(r => r.Errors)
                 .ToList();
-            var errors = new List<Error>();
+
             if (failures.Count > 0)
                 throw new FluentValidation.ValidationException(failures);
         }
-        return await next();
+        return await next().ConfigureAwait(false);
     }
 
+    private async Task<ValidationFailure[]> ValidateAsync(TRequest request)
+    {
+        if (!validators.Any())
+            return [];
+
+        var context = new ValidationContext<TRequest>(request);
+
+
+        var validationResults = await Task.WhenAll(
+        validators.Select(v =>
+                   v.ValidateAsync(context)));
+
+        var failures = validationResults
+                .Where(r => !r.IsValid)
+                .SelectMany(r => r.Errors)
+                .ToArray();
+
+        return failures;
+
+    }
 }
